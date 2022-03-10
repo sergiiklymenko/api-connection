4674ef7bf03dcf46bd723f66c53c6d19b75f6b6f
sklymenko <mikki.rossa@gmail.com>
2022-03-10 13:29:48 -0500

    Creating build for iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.serg.app',
+  appName: 'apiconnection',
+  webDir: 'dist/apiconnection',
+  bundledWebRuntime: false
+};
+
+export default config;
